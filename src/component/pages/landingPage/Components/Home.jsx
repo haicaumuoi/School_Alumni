c15386a9a2940c1aec049d4a9701be52e61f2cd0
kimdat0705 <kimdat0705@gmail.com>
2023-06-07 23:58:43 +0700

    Dat update info in home page

--- src/component/pages/landingPage/Components/Home.jsx
+++ src/component/pages/landingPage/Components/Home.jsx
@@ -6,8 +6,17 @@ import { FiArrowRight } from "react-icons/fi";
 import { Link } from "react-router-dom";
 import Typography from "antd/es/typography/Typography";
 import LoginPage from "../../loginPage/LoginPage";
+import { Carousel } from "antd";
 
-const {Title} =  Typography;
+const { Title } = Typography;
+
+const contentStyle = {
+  height: "160px",
+  color: "#fff",
+  lineHeight: "160px",
+  textAlign: "center",
+  background: "#364d79",
+};
 
 const Home = () => {
   return (
@@ -19,15 +28,21 @@ const Home = () => {
         </div>
         <div className="home-text-section">
           {/* <h1 className="primary-heading">School Alumni - Super ADMIN</h1> */}
-          <Title level={1} className="main-title-section">School Alumni - Super ADMIN</Title>
+          <Title level={1} className="main-title-section">
+            School Alumni - Super ADMIN
+          </Title>
           {/* <p className="primary-text">Manage education pages</p> */}
-          <Title level={4} className="second-title-section">Manage education pages</Title>
+          <Title level={4} className="second-title-section">
+            Offering a range of features and tools to support alumni
+            interactions, including news, events, fundraising, job sharing
+            functions.
+          </Title>
 
           <button className="secondary-button">
             <Link to="/login">
               <Typography>
                 Getting Started <FiArrowRight />
-                </Typography>
+              </Typography>
             </Link>
           </button>
 
